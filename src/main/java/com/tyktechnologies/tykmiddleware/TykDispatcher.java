c3ff8f7b21a97677e46cbad5f22a3b6355187895
Sedky Abou-Shamalah <sedkya@outlook.com>
2019-05-29 13:16:22 -0400

    Change the structure to work as an authentication piece, and upgrading gradle to work the 5.4 version

--- src/main/java/com/tyktechnologies/tykmiddleware/TykDispatcher.java
+++ src/main/java/com/tyktechnologies/tykmiddleware/TykDispatcher.java
@@ -2,39 +2,54 @@ package com.tyktechnologies.tykmiddleware;
 
 import coprocess.DispatcherGrpc;
 import coprocess.CoprocessObject;
+import coprocess.CoprocessSessionState;
+import coprocess.CoprocessReturnOverrides;
 
 public class TykDispatcher extends DispatcherGrpc.DispatcherImplBase {
+
+    final String FOOBAR = "foobar";
 
     @Override
     public void dispatch(CoprocessObject.Object request,
             io.grpc.stub.StreamObserver<CoprocessObject.Object> responseObserver) {
 
         System.out.println("*** Incoming Request ***");
-        System.out.println(request.toString());
+        System.out.println("Hook name: " + request.getHookName());
 
-        CoprocessObject.Object modifiedRequest = null;
+        final CoprocessObject.Object modifiedRequest = MyAuthHook(request);
+        responseObserver.onNext(modifiedRequest);
 
-        switch (request.getHookName()) {
-            case "MyPreMiddleware":
-                modifiedRequest = MyPreHook(request);
-            default:
-            // Do nothing, the hook name isn't implemented!
-        }
-
-        // Return the modified request (if the transformation was done):
-        if (modifiedRequest != null) {
-            responseObserver.onNext(modifiedRequest);
-
-            System.out.println("*** Transformed Request ***");
-            System.out.println(modifiedRequest.toString());
-        };
+        System.out.println("*** Transformed Request ***");
 
         responseObserver.onCompleted();
     }
 
-    CoprocessObject.Object MyPreHook(CoprocessObject.Object request) {
+    CoprocessObject.Object MyAuthHook(CoprocessObject.Object request) {
+        String authHeader = request.getRequest().getHeadersOrDefault("Authorization", "");
+        if(!authHeader.equals(FOOBAR)) {
+            CoprocessObject.Object.Builder builder = request.toBuilder();
+            CoprocessReturnOverrides.ReturnOverrides retOverrides = CoprocessReturnOverrides.ReturnOverrides.newBuilder()
+            .setResponseCode(403)
+            .setResponseError("Not authorized")
+            .build();
+
+            builder.getRequestBuilder().setReturnOverrides(retOverrides);
+            return builder.build();
+        }
+
+        final long expiryTime = (System.currentTimeMillis() / 1000) + 5;
+
+        CoprocessSessionState.SessionState session = CoprocessSessionState.SessionState.newBuilder()
+        .setRate(1000.0)
+        .setPer(1.0)
+        .setIdExtractorDeadline(expiryTime)
+        .build();
+
         CoprocessObject.Object.Builder builder = request.toBuilder();
-        builder.getRequestBuilder().putSetHeaders("customheader", "customvalue");
+        // Mandatory fields
+        builder.putMetadata("token", FOOBAR);  // Used for downstream chains
+        builder.setSession(session);
+
         return builder.build();
     }
 }
